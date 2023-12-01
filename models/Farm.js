a8297ec20a80dbcdc5c707809d7bebe86ad3e3e7
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-11-30 19:18:23 -0600

    update to models

---
+++ models/Farm.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Farm extends Model { }
+
+Farm.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        needed_funding: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'farm',
+    }
+);
+
+module.exports = Farm;
