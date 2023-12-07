aff50b7d55090e40c9faf73bf9af91c2b28ca547
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-12-06 18:02:02 -0600

    update to homepage route and product route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,12 +3,43 @@ const { Farm } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get('/login', async (req, res) => {
-    console.log(123);
     try {
-        res.render('login');
+        const farmData = await Farm.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        const farms = farmData.map((farm) => farm.get({ plain: true }));
+        res.render('homepage', {
+            farms,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/farm/:id', async (req, res) => {
+    try {
+        const farmData = await Farm.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        const farm = farmData.get({ plain: true });
+        res.render('farm', {
+            ...farm,
+            logged_in: req.session.logged_in
+        });
     }catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 module.exports = router;
