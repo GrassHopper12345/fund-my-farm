2588e16e894ed9fedde465a3ef077e5b3b60443d
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-12-11 17:37:36 -0600

    update

--- models/ProductToFarm.js
+++ models/ProductToFarm.js
@@ -1,40 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
+// const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection');
+// const sequelize = require('../config/connection');
 
-class ProductToFarm extends Model { }
+// class ProductToFarm extends Model { }
 
-ProductToFarm.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id',
-      }
-    },
-    farm_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'farm',
-        key: 'id',
-      }
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'producttofarm',
-  }
-);
+// ProductToFarm.init(
+//   {
+//     // define columns
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     product_id: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: 'product',
+//         key: 'id',
+//       }
+//     },
+//     farm_id: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: 'farm',
+//         key: 'id',
+//       }
+//     }
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'producttofarm',
+//   }
+// );
 
-module.exports = ProductToFarm;
+// module.exports = ;
